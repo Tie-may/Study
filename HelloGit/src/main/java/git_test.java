f0f768bb94695ebfa23a53796806738d14e82527
Tie-may <68210382+Tie-may@users.noreply.github.com>
2020-09-16 16:46:48 +0800

    Update git_test.java

--- HelloGit/src/main/java/git_test.java
+++ HelloGit/src/main/java/git_test.java
@@ -5,5 +5,6 @@ public class git_test {
         System.out.println("master_2");
         System.out.println("master_3");
         System.out.println("master_4");
+        System.out.println("master_5");
     }
 }
