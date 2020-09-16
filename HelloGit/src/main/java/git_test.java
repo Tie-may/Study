016fb9f37711dbff586c702f16c4b2dec0928a30
tie_may <zhangxiang2233@gmail.com>
2020-09-16 16:27:47 +0800

    master_3

--- HelloGit/src/main/java/git_test.java
+++ HelloGit/src/main/java/git_test.java
@@ -3,5 +3,6 @@ public class git_test {
         System.out.println("hello git");
         System.out.println("master_1");
         System.out.println("master_2");
+        System.out.println("master_3");
     }
 }
