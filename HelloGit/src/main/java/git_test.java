164c1249115a34233c9c9494f8341a71546214a1
tie_may <zhangxiang2233@gmail.com>
2020-09-16 16:24:31 +0800

    创建2

--- HelloGit/src/main/java/git_test.java
+++ HelloGit/src/main/java/git_test.java
@@ -2,5 +2,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("master_1");
+        System.out.println("master_2");
     }
 }
